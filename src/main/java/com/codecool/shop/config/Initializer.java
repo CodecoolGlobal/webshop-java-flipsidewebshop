0706c18badc7349e4d921bca99ba47a6e089a48d
dr. Simon Bálint <balladin0@gmail.com>
2020-03-31 09:32:27 +0200

    add multiple product categories to main page

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -28,14 +28,23 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(amazon);
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
+        // boards - new stuff
+        Supplier burton = new Supplier("Burton", "Snowboards");
+        supplierDataStore.add(burton);
+        Supplier jones = new Supplier("Jones", "Snowboards");
+        supplierDataStore.add(jones);
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
+        ProductCategory snowboard = new ProductCategory("Snowboard", "Winter", "A piece of fiberglass, P-TEX, wood compound that makes you surf the slopes in winter.");
+        productCategoryDataStore.add(snowboard);
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Custom", 300, "USD", "All times classic one-quiver all-mountain camber board that fits every rider.", snowboard, burton));
+        System.out.println(productCategoryDataStore.getAll());
     }
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -14,11 +16,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
+
+    private Object ArrayList;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -27,8 +33,25 @@ public class ProductController extends HttpServlet {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productCategoryDataStore.find(1));
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+
+        context.setVariable("categories", productCategoryDataStore.getAll());
+        /*for (ProductCategory product: productCategoryDataStore.getAll()) {
+            System.out.println(product.getName());
+            System.out.println(product.getId());
+            System.out.println(product.getDepartment());
+        }*/
+        context.setVariable("products", productDataStore.getAll());
+
+        for (Product product: productDataStore.getAll()) {
+            System.out.println(product.getName());
+            System.out.println(product.getId());
+            System.out.println(product.getProductCategory().getId());
+        }
+
+        /*Original version:
+        context.setVariable("category", productCategoryDataStore.find(1)); // TODO: find out how to add more categories
+        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));*/
+
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
         // params.put("category", productCategoryDataStore.find(1));
