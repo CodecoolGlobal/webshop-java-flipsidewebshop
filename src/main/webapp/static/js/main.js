7d8db508647a3d841edd83dd85a86f5bc04191ed
dr. Simon Bálint <balladin0@gmail.com>
2020-03-31 23:04:49 +0200

    delete unneeded code that caused TypeError

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -14,13 +14,11 @@ function getAllProducts() {
 
 function openNav() {
     document.getElementById("mySidebar").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
 }
 
 getAllProducts();
