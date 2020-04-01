675b8fe3b8bdc005237328ef73abab12aaf27831
Rhyvan <polyakan@gmail.com>
2020-04-01 22:39:10 +0200

    changes, nothing working properly

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -10,10 +10,11 @@ function main() {
 
 function fetchPostMethod(url, content, callback, errorCallback) {
     console.log(content);
+
     fetch(url, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(content)
+        body: JSON.stringify({'id': content.id, 'amount': content.amount })
     })
         .then((resp) => {return resp.json()})
         .then((data) => callback(data, content))
@@ -23,20 +24,62 @@ function fetchPostMethod(url, content, callback, errorCallback) {
 function addNewLineToModalBody(response, data) {
     let modal = document.querySelector('.modal-body');
     console.log(response);
-    modal.innerHTML += `
-<div>
-    We have of item with id ${data.id}, added amount of ${data.amount} and the response is ${response}. 
-</div>`
+    if (response){
+        modal.innerHTML += `
+            <div>
+                We have of item with id ${data.id}, added amount of ${data.amount}
+                and the response is ${response}. 
+            </div>`;
+        modal.insertAdjacentHTML("beforeend", template.modal(data))
+    }
 }
 
 function fetchError() {
     console.log("error");
 }
 
+function gatherData(target){
+    console.log(target.dataset.id);
+    let storedData = {
+        "id" : target.dataset.id,
+        "price" : target.parentNode.querySelector(".price").dataset.price,
+        "description" :  target.closest(".product").querySelector(".description").dataset.description,
+        "name":target.closest(".product").querySelector(".card-title").dataset.name
+    };
+    return storedData;
+}
+
+
 function addOneItemToCart(addButton) {
-    addButton.addEventListener("click", function () {
-        let id = Number(addButton.dataset.id);
-        let data = {'id': id, 'amount': 1};
+    addButton.addEventListener("click", (event) => {
+        //let id = Number(addButton.dataset.id);
+        //let data = {'id': id, 'amount': 1};
+        let data = gatherData(event.target.parentElement);
+        data.amount = 1;
         fetchPostMethod(`/api/add-to-cart`, data, addNewLineToModalBody, fetchError)
     })
+}
+
+let template = {
+    modal: (product) => {
+        return `
+        <div class="itemwrapper" style="display:inline-flex">
+            <div class="picture" style="width:200px;height:200px">
+                <img class=""
+                     src='/static/img/product_${product.id}.jpg' alt=""/>
+            </div>
+            <div class="infoaboutitem" style="display:flex;flex-direction: column">
+                <div>${product.name}</div>
+                <div>${product.price}">Price</div>
+                <div>
+                    <button name="minusitem" class="minus-item" data-id="${product.id}">Minus</button>
+                    <div class="amount">${product.amount}</div>
+                    <button name="plusitem" class="plus-item"  data-id="${product.id}">Plus</button>
+                    <button name="deleteitem" class="del-item" data-id="${product.id}">Del</button>
+                </div>
+            </div>
+        </div>
+        
+        `
+    },
 }
--- src/main/webapp/static/js/cartButtons.js
+++ src/main/webapp/static/js/cartButtons.js
@@ -1,67 +1,71 @@
 main();
 
 function main() {
-    let addButtons = document.querySelectorAll(".plus-item");
-    for (let addButton of addButtons) {
-        console.log("plus");
-        addOneMoreItemToCart(addButton);
+    document.querySelector(".modal-body").addEventListener("click", (event) => {
+        if (event.target.matches(".plus-item")) {
+            addOneMoreItemToCart(event.target)
+        }
+        if (event.target.matches(".minus-item")) {
+            removeOneItemFromCart(event.taget)
+        }
+        if (event.target.matches(".delete-item")) {
+            removeOneItemFromCart(event.target)
+        }
+    });
+
+    /*
+        let addButtons = document.querySelectorAll(".plus-item");
+        for (let addButton of addButtons) {
+            console.log("plus");
+            addOneMoreItemToCart(addButton);
+        }
+
+        let removeButtons = document.querySelectorAll(".minus-item");
+        for (let removeButton of removeButtons) {
+            console.log("minus");
+            removeOneItemFromCart(removeButton);
+        }
     }
 
-    let removeButtons = document.querySelectorAll(".minus-item");
-    for (let removeButton of removeButtons) {
-        console.log("minus");
-        removeOneItemFromCart(removeButton);
+     */
+
+
+    function fetchPostMethod(url, content, callback, errorCallback) {
+        console.log(content);
+        fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(content)
+        })
+            .then((resp) => {
+                return resp.json()
+            })
+            .then((data) => callback(data, content))
+            .catch(errorCallback);
     }
-}
 
+    function fetchError() {
+        console.log("error");
+    }
 
-function fetchPostMethod(url, content, callback, errorCallback) {
-    console.log(content);
-    fetch(url, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(content)
-    })
-        .then((resp) => {return resp.json()})
-        .then((data) => callback(data, content))
-        .catch(errorCallback);
-}
-
-function addNewLineToModalBody(response, data) {
-    let modal = document.querySelector('.modal-body');
-    console.log(response);
-    modal.innerHTML += `
-<div>
-    We have of item with id ${data.id}, added amount of ${data.amount} and the response is ${response}. 
-</div>`
-}
-
-function fetchError() {
-    console.log("error");
-}
-
-function addOneMoreItemToCart(button) {
-    button.addEventListener("click", function () {
+    function addOneMoreItemToCart(button) {
         let id = Number(button.dataset.id);
         let amount = Number(button.dataset.amount);
         let data = {'id': id, 'amount': 1};
         console.log(`id: ${data.id}, amount: ${data.amount}`);
         fetchPostMethod('api/add-to-cart', data, addNewLineToModalBody, fetchError)
-    })
-}
 
-function removeOneItemFromCart(button) {
-    button.addEventListener("click", function () {
+    }
+
+    function removeOneItemFromCart(button) {
         let id = Number(button.dataset.id);
         let amount = Number(button.dataset.amount);
         let data = {'id': id, 'amount': -1};
         console.log(`id: ${data.id}, amount: ${data.amount}`);
         fetchPostMethod('api/remove-item', data, addNewLineToModalBody, fetchError)
-    })
-}
+    }
 
-function emptyCart(emptyButton) {
-    emptyButton.addEventListener("click", function () {
+    function emptyCart() {
         fetchPostMethod('api/empty-cart', {'id': 0}, addNewLineToModalBody, fetchError)
-    })
+    }
 }
