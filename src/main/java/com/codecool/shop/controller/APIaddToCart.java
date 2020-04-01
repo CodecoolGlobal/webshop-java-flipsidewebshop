1d56d0b4a3b64e834ec0caa4325c9da230dac814
dr. Simon Bálint <balladin0@gmail.com>
2020-04-01 10:58:49 +0200

    add unstable version for Benec to see

--- src/main/java/com/codecool/shop/controller/APIaddToCart.java
+++ src/main/java/com/codecool/shop/controller/APIaddToCart.java
@@ -8,15 +8,20 @@ import com.codecool.shop.model.Customer;
 import com.codecool.shop.model.Item;
 import com.codecool.shop.model.Product;
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/api/add-to-cart"})
 public class APIaddToCart extends HttpServlet {
@@ -24,8 +29,25 @@ public class APIaddToCart extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
         Customer customer = new Customer(); // TODO: get existing customer instance!
-        Cart cart = customer.getCartInstance();
-        System.out.println();
+        //Cart cart = customer.getCartInstance();
+        System.out.println(request.getParameterMap());
+
+        //StringBuffer jb = new StringBuffer();
+        try {
+            Reader reader = request.getReader();
+            //String  line = request.getParameter("id");
+            Gson gson = new Gson();
+            //String returned = line.toString();
+            String object = gson.fromJson(reader, String.class);
+            System.out.println("were here " + object); // mit adjak neki?
+
+        } catch (Error e) {
+            // crash and burn
+            System.out.println(e);
+        }
+
+
+
 
 
         int id = Integer.parseInt(request.getParameter("id"));
