a0d6ff1c9077e852bf90d304960ed39c63a6861b
dr. Simon Bálint <balladin0@gmail.com>
2020-04-01 14:41:02 +0200

    add epty cart and remove item capabilities

---
+++ src/main/java/com/codecool/shop/controller/APIEmptyCart.java
@@ -0,0 +1,37 @@
+package com.codecool.shop.controller;
+
+
+import com.codecool.shop.controller.json.requestRemoveContainer;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Customer;
+import com.codecool.shop.model.Product;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
+
+@WebServlet(urlPatterns = {"/api/remove-item"})
+public class APIEmptyCart extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+        /* method body*/
+        Customer customer = new Customer(); // TODO: get existing customer instance!
+        customer.emptyCart();
+
+        /* Generate and send response */
+        String JSONrepsonse = new Gson().toJson(true);
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        out.print(JSONrepsonse);
+        out.flush();
+    }
+}
