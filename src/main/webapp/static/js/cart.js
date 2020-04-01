ee39e1a90e38bc814a200061f64300e19e4e827a
dr. Simon Bálint <balladin0@gmail.com>
2020-04-01 18:41:36 +0200

    fixes amounts to numbers

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -5,16 +5,6 @@ function main() {
     for (let cartButton of cartButtons) {
         addOneItemToCart(cartButton);
     }
-
-    let addButtons = document.querySelectorAll(".plus-item");
-    for (let addButton of addButtons) {
-        removeOneItemFromCart(addButton);
-    }
-
-    let removeButtons = document.querySelectorAll(".minus-item");
-    for (let removeButton of removeButtons) {
-        removeOneItemFromCart(removeButton);
-    }
 }
 
 
@@ -48,32 +38,5 @@ function addOneItemToCart(addButton) {
         let id = addButton.dataset.id;
         let data = {'id': id, 'amount': 1};
         fetchPostMethod(`/api/add-to-cart`, data, addNewLineToModalBody, fetchError)
-    })
-}
-
-function addOneMoreItemToCart(addButton) {
-    addButton.addEventListener("click", function () {
-        let id = addButton.dataset.id;
-        let amount = addButton.dataset.amount;
-        let data = {'id': id, 'amount': amount+1};
-        fetchPostMethod('api/add-to-cart', data, addNewLineToModalBody, fetchError)
-    })
-}
-
-function removeOneItemFromCart(removeButton) {
-    removeButton.addEventListener("click", function () {
-        let id = removeButton.dataset.id;
-        let amount = removeButton.dataset.amount;
-        if (amount < 1) {
-            amount = 1;
-        }
-        let data = {'id': id, 'amount': amount-1};
-        fetchPostMethod('api/remove-item', data, addNewLineToModalBody, fetchError)
-    })
-}
-
-function emptyCart(emptyButton) {
-    emptyButton.addEventListener("click", function () {
-        fetchPostMethod('api/empty-cart', {'id': 0}, addNewLineToModalBody, fetchError)
     })
 }
---
+++ src/main/webapp/static/js/cartButtons.js
@@ -0,0 +1,70 @@
+main();
+
+function main() {
+    let addButtons = document.querySelectorAll(".plus-item");
+    for (let addButton of addButtons) {
+        console.log("plus");
+        addOneMoreItemToCart(addButton);
+    }
+
+    let removeButtons = document.querySelectorAll(".minus-item");
+    for (let removeButton of removeButtons) {
+        console.log("minus");
+        removeOneItemFromCart(removeButton);
+    }
+}
+
+
+function fetchPostMethod(url, content, callback, errorCallback) {
+    console.log(content);
+    fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(content)
+    })
+        .then((resp) => {return resp.json()})
+        .then((data) => callback(data, content))
+        .catch(errorCallback);
+}
+
+function addNewLineToModalBody(response, data) {
+    let modal = document.querySelector('.modal-body');
+    console.log(response);
+    modal.innerHTML += `
+<div>
+    We have of item with id ${data.id}, added amount of ${data.amount} and the response is ${response}. 
+</div>`
+}
+
+function fetchError() {
+    console.log("error");
+}
+
+function addOneMoreItemToCart(button) {
+    button.addEventListener("click", function () {
+        let id = Number(button.dataset.id);
+        let amount = Number(button.dataset.amount);
+        let data = {'id': id, 'amount': amount+1};
+        console.log(`id: ${data.id}, amount: ${data.amount}`);
+        fetchPostMethod('api/add-to-cart', data, addNewLineToModalBody, fetchError)
+    })
+}
+
+function removeOneItemFromCart(button) {
+    button.addEventListener("click", function () {
+        let id = Number(button.dataset.id);
+        let amount = Number(button.dataset.amount);
+        if (amount < 1) {
+            amount = 1;
+        }
+        let data = {'id': id, 'amount': amount-1};
+        console.log(`id: ${data.id}, amount: ${data.amount}`);
+        fetchPostMethod('api/remove-item', data, addNewLineToModalBody, fetchError)
+    })
+}
+
+function emptyCart(emptyButton) {
+    emptyButton.addEventListener("click", function () {
+        fetchPostMethod('api/empty-cart', {'id': 0}, addNewLineToModalBody, fetchError)
+    })
+}
