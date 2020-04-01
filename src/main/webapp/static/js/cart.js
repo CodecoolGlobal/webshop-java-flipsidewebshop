03dd4acba22f8d390b0083a2f3ca4e320b933148
dr. Simon Bálint <balladin0@gmail.com>
2020-04-01 18:02:17 +0200

    add event listeners to all buttons

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -5,12 +5,22 @@ function main() {
     for (let cartButton of cartButtons) {
         addOneItemToCart(cartButton);
     }
+
+    let addButtons = document.querySelectorAll(".plus-item");
+    for (let addButton of addButtons) {
+        removeOneItemFromCart(addButton);
+    }
+
+    let removeButtons = document.querySelectorAll(".minus-item");
+    for (let removeButton of removeButtons) {
+        removeOneItemFromCart(removeButton);
+    }
 }
 
 
-function fetchPostMethod(content, callback, errorCallback) {
+function fetchPostMethod(url, content, callback, errorCallback) {
     console.log(content);
-    fetch(`/api/add-to-cart`, {
+    fetch(url, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(content)
@@ -37,6 +47,33 @@ function addOneItemToCart(addButton) {
     addButton.addEventListener("click", function () {
         let id = addButton.dataset.id;
         let data = {'id': id, 'amount': 1};
-        fetchPostMethod(data, addNewLineToModalBody, fetchError)
+        fetchPostMethod(`/api/add-to-cart`, data, addNewLineToModalBody, fetchError)
+    })
+}
+
+function addOneMoreItemToCart(addButton) {
+    addButton.addEventListener("click", function () {
+        let id = addButton.dataset.id;
+        let amount = addButton.dataset.amount;
+        let data = {'id': id, 'amount': amount+1};
+        fetchPostMethod('api/add-to-cart', data, addNewLineToModalBody, fetchError)
+    })
+}
+
+function removeOneItemFromCart(removeButton) {
+    removeButton.addEventListener("click", function () {
+        let id = removeButton.dataset.id;
+        let amount = removeButton.dataset.amount;
+        if (amount < 1) {
+            amount = 1;
+        }
+        let data = {'id': id, 'amount': amount-1};
+        fetchPostMethod('api/remove-item', data, addNewLineToModalBody, fetchError)
+    })
+}
+
+function emptyCart(emptyButton) {
+    emptyButton.addEventListener("click", function () {
+        fetchPostMethod('api/empty-cart', {'id': 0}, addNewLineToModalBody, fetchError)
     })
 }
