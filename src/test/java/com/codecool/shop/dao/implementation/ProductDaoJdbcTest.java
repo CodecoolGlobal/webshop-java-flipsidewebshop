2bfd6b80fb754e10c8ef78b615f6d317995801af
dr. Simon Bálint <balladin0@gmail.com>
2020-04-15 20:51:42 +0200

    add initial test setup

--- src/test/java/com/codecool/shop/dao/implementation/ProductDaoJdbcTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/ProductDaoJdbcTest.java
@@ -1,5 +1,9 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +13,18 @@ class ProductDaoJdbcTest {
 
     @Test
     void testIsProductAdded() {
+        Supplier DGK = new Supplier("DGK", "Skateboards");
+        ProductCategory skateboard = new ProductCategory("Skateboard", "Summer",
+                "A short narrow board with two small wheels fixed to the bottom of either end, " +
+                        "on which a person can ride pushing one foot against the ground.");
+        Product product = new Product("Bruce Lee", 69.95f, "USD",
+                "Fantastic price. You can truly skate with the power of Bruce Lee", skateboard, DGK);
+
+        ProductDao productDaoJdbc = ProductDaoJdbc.getInstance();
+        productDaoJdbc.add(product);
+
+        System.out.println(productDaoJdbc.getBy(DGK));
+
 
 
 
