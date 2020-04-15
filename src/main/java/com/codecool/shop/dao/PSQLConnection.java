ccf9fdbce5126e9c056f798274b953183a9a5132
dr. Simon Bálint <balladin0@gmail.com>
2020-04-15 10:24:58 +0200

    initial commit for JDBC connection

---
+++ src/main/java/com/codecool/shop/dao/PSQLConnection.java
@@ -0,0 +1,59 @@
+package com.codecool.shop.dao;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PSQLConnection {
+
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/yourdatabasename"; // TODO: update
+    private static final String DB_USER = "username"; // TODO: update
+    private static final String DB_PASSWORD = "pswd"; // TODO: update
+
+    private static PSQLConnection dbIsntance;
+    private static Connection connection;
+    private static Statement stmt;
+
+
+    private PSQLConnection() {
+    }
+
+    public static PSQLConnection getInstance() {
+        if (dbIsntance == null) {
+            dbIsntance = new PSQLConnection();
+        }
+        return dbIsntance;
+    }
+
+    public Connection getConnection() {
+
+        if (connection == null) {
+            try {
+                String host = DATABASE;
+                String username = DB_USER;
+                String password = DB_PASSWORD;
+                connection = DriverManager.getConnection(host, username, password);
+            } catch (SQLException ex) {
+                Logger.getLogger(PSQLConnection.class.getName()).log(Level.SEVERE, null, ex);
+                ex.printStackTrace();
+            }
+        }
+
+        return connection;
+    }
+
+    private void executeQuery(String query) {
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+        ){
+            statement.execute(query);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(PSQLConnection.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
+    }
+}
