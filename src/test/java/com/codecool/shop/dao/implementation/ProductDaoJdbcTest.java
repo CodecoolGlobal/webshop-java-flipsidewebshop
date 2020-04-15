a392d64ff179c7caeee04db0613a030608ed98cf
dr. Simon Bálint <balladin0@gmail.com>
2020-04-15 22:54:16 +0200

    add rudimentary pre-test config

--- src/test/java/com/codecool/shop/dao/implementation/ProductDaoJdbcTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/ProductDaoJdbcTest.java
@@ -7,13 +7,16 @@ import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductDaoJdbcTest {
 
     @Test
     void testIsProductAdded() {
-        Supplier DGK = new Supplier("DGK", "Skateboards");
+        Supplier DGK = new Supplier("DGK", "Skateboards"); // should add to SQL!
+
         ProductCategory skateboard = new ProductCategory("Skateboard", "Summer",
                 "A short narrow board with two small wheels fixed to the bottom of either end, " +
                         "on which a person can ride pushing one foot against the ground.");
@@ -21,12 +24,18 @@ class ProductDaoJdbcTest {
                 "Fantastic price. You can truly skate with the power of Bruce Lee", skateboard, DGK);
 
         ProductDao productDaoJdbc = ProductDaoJdbc.getInstance();
+        System.out.println(productDaoJdbc.toString());
         productDaoJdbc.add(product);
+        System.out.println("We added a product");
+        DGK.setId(1); // once in SQL, no need to add it here
+        List<Product> testProduct = productDaoJdbc.getBy(DGK);
+        System.out.println(testProduct.toString());
+        System.out.println("We got a product");
 
-        System.out.println(productDaoJdbc.getBy(DGK));
-
-
-
+        for (int i = 0; i < testProduct.size(); i++) {
+            System.out.println(testProduct.get(i));
+            System.out.println("We are here");
+        }
 
 
     }
