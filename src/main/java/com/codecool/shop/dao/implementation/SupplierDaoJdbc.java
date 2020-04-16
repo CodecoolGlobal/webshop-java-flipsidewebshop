e664813aec636be0b324f7bd0f163dc4ae96a885
dr. Simon Bálint <balladin0@gmail.com>
2020-04-16 08:14:26 +0200

    add add()

--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
@@ -3,6 +3,9 @@ package com.codecool.shop.dao.implementation;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SupplierDaoJdbc implements SupplierDao {
@@ -18,10 +21,20 @@ public class SupplierDaoJdbc implements SupplierDao {
         return instance;
     }
 
-
     @Override
     public void add(Supplier supplier) {
+        psqlConnection = PSQLConnection.getInstance();
+        String sql = "INSERT INTO supplier (name, description)" +
+                " VALUES(?,?)";
 
+        try (Connection conn = psqlConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, supplier.getName());
+            pstmt.setString(2, supplier.getDescription());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
