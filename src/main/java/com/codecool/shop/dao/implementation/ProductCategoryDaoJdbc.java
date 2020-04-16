23befcb8c8dc73678d738c5fddb504402dd3fbdc
dr. Simon Bálint <balladin0@gmail.com>
2020-04-16 07:54:28 +0200

    add add() to Product Category DAO JDBC

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
@@ -3,6 +3,9 @@ package com.codecool.shop.dao.implementation;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ProductCategoryDaoJdbc implements ProductCategoryDao {
@@ -20,7 +23,19 @@ public class ProductCategoryDaoJdbc implements ProductCategoryDao {
 
     @Override
     public void add(ProductCategory category) {
+        psqlConnection = PSQLConnection.getInstance();
+        String sql = "INSERT INTO product_category (name, description, department)" +
+                " VALUES(?,?,?)";
 
+        try (Connection conn = psqlConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, category.getName());
+            pstmt.setString(2, category.getDescription());
+            pstmt.setString(3, category.getDepartment());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
